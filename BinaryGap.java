9d33d65b43d4affab7dabb7bbd82e2ef90064e03
styro103 <smbateng103@yahoo.com>
2016-02-03 21:30:15 -0500

    Second Solution Uploaded
    Quicker, more effective solution pushed.

--- BinaryGap.java
+++ BinaryGap.java
@@ -1,30 +1,36 @@
+/*
+	Solution to Codility's Binary Gap Task
+	O(log(N))
+*/
 class Solution 
 {
     public int solution(int N) 
     {
         //Base and Edge Cases
-	if (N<5)
-            return 0;
-        else if (N==5)
-            return 1;
-	else if (N<9)
-	    return 0;
-        
-        String bin = Integer.toBinaryString(N); //Binary String Representation of Integer
+		if (N<5)
+			return 0;
+		else if (N==5)
+			return 1;
+		else if (N<9)
+			return 0;
+		
         int mc = 0; //Max Count
-        int cc = 0; //Count
-        
-        for (int i=1; i<bin.length(); i++) //Loop Through Binary String From 2nd Element
+		int cc = 0; //Count
+		
+        while (N>0 && N%2==0) //Remove Trailing Zeroes
+            N/=2;
+        while (N>0) //Check Digits
         {
-            if (bin.charAt(i)=='0')
-                cc++; //Increment Count if Digit is 0
-            else if (cc>0) //Digit is 1, If Count Isn't Zero
+            if (N%2==0) //If Digit is 0
+                cc++; //Increment Count
+            else if (cc!=0) //Digit is 1, If Count Isn't Zero
             {
                 mc = (cc>mc) ? cc : mc; //Set Max Count if Count is Greater
-                cc = 0; //Reset Count
+				cc = 0; //Reset Count
             }
+            
+            N/=2; //Set Next Digit to Check
         }
-        
         return mc; //Return Max Count
     }
 }
---
+++ BinaryGap2.java
@@ -0,0 +1,34 @@
+/*
+	Solution to Codility's Binary Gap Task
+	Run Time is Number of Digits of Input's Binary Representation
+*/
+class Solution 
+{
+    public int solution(int N) 
+    {
+        //Base and Edge Cases
+		if (N<5)
+			return 0;
+		else if (N==5)
+			return 1;
+		else if (N<9)
+			return 0;
+			
+		String bin = Integer.toBinaryString(N); //Binary String Representation of Integer
+		int mc = 0; //Max Count
+		int cc = 0; //Count
+			
+		for (int i=1; i<bin.length(); i++) //Loop Through Binary String From 2nd Element
+		{
+			if (bin.charAt(i)=='0')
+			   cc++; //Increment Count if Digit is 0
+			else if (cc>0) //Digit is 1, If Count Isn't Zero
+			{
+				mc = (cc>mc) ? cc : mc; //Set Max Count if Count is Greater
+				cc = 0; //Reset Count
+			}
+		}
+			
+		return mc; //Return Max Count
+    }
+}
